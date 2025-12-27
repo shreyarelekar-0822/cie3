d408ae053a14af7373e105c6be9d34495228cf33
shreyarelekar-0822 <shreyarelekar0822@gmail.com>
2025-12-27 12:51:01 +0530

    Add test for vehicle_details function

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+from vehicle import vehicle_details
 
+def test_vehicle_details():
+    expected_output = (
+        "v_id:143\n"
+        "v_name:mustang\n"
+        "price:6000\n"
+        "year_of_purchase:2001\n"
+    )
+    assert vehicle_details("143", "mustang", 6000, "2001") == expected_output
