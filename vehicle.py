7c0fa49fd1bec4f6e68104909ae581c472c45369
shreyarelekar-0822 <shreyarelekar0822@gmail.com>
2025-12-27 12:59:32 +0530

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -1,1 +1,15 @@
+def vehicle_details(v_id, v_name, price, year_of_purchase):
+    result = (
+        f"v_id: {v_id}\n"
+        f"v_name: {v_name}\n"
+        f"price: {price}\n"
+        f"year_of_purchase: {year_of_purchase}\n"
+    )
+    return result
 
+if __name__ == "__main__":
+    v_id = "143"
+    v_name = "mustang"
+    price = "6000"
+    year_of_purchase = "2001"
+    print(vehicle_details(v_id, v_name, price, year_of_purchase))
